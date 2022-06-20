2128451c8b06e4516dddb53cccc743a7a081377a
Giossaurus <gihhskt@gmail.com>
2022-06-19 23:26:22 -0300

    substituição com várias funções (jeito dificil)

--- main.js
+++ main.js
@@ -27,3 +27,30 @@ function tocaSomSplash(){
 }
 
 document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+function tocaSomPsh(){
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+

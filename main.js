45af4b9065123cc249d74fffeb8e31c89a57a5f5
Giossaurus <gihhskt@gmail.com>
2022-06-19 23:44:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-// function tocaSomPom(){
-//     document.querySelector('#som_tecla_pom').play();
-// }
-
-// document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;

ba9d7980351b6441f240ecad383c1e08c5c132fa
Giossaurus <gihhskt@gmail.com>
2022-06-19 23:41:10 -0300

    referencia constante armazenando teclas

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
-}
+// function tocaSomPom(){
+//     document.querySelector('#som_tecla_pom').play();
+// }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
- 
+// document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');

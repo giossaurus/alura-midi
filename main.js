2e46abbbd87e8ff7d9b688a187ce4425293f6712
Giossaurus <gihhskt@gmail.com>
2022-07-18 22:51:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ let contador = 0;
 
 while (contador < listaDeTeclas.length) {
 
+    const instrumento = listaDeTeclas[contador].classList[1];
+
+    console.log(instrumento);
+
     listaDeTeclas[contador].onclick = function () {
         tocaSom('#som_tecla_pom');
     };

980285dd8ccf929e11be42e28725c004e492e4fe
Giossaurus <gihhskt@gmail.com>
2022-07-18 22:54:20 -0300

    template string

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ let contador = 0;
 
 while (contador < listaDeTeclas.length) {
 
-    const instrumento = listaDeTeclas[contador].classList[1];
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
 
     console.log(instrumento);
 
-    listaDeTeclas[contador].onclick = function () {
+    tecla.onclick = function () {
         tocaSom('#som_tecla_pom');
     };
 

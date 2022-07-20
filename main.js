4aa1db9e8ae239e52643ea3c3134970dc27f52a6
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2022-07-20 01:00:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,12 +15,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     }
 
     tecla.onkeydown = function (evento) {
-        console.log(evento.code)
-
-        if (evento.code == 'Space') {
+        
+        if (evento.code === 'Space') {
             tecla.classList.add('ativa');
         }
-        
+
+        if (evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
     }
 
     tecla.onkeyup = function () {
